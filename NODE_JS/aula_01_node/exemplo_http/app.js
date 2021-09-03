9acfd6b014a988eb866865d588fe802a97b1e244
rfka <kaminski.lista@gmail.com>
2021-09-02 21:47:47 -0300

    Delete NODE_JS directory

--- NODE_JS/aula_01_node/exemplo_http/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-// console.log(http);
-
-http.createServer(function(req, res){
-    res.end('<h1>Ola Mundo!!</h1>');
-}).listen(3000);
-
-console.log('Meu servidor esta rodando');
--- NODE_JS/aula_01_node/mini_calculadora/app.js
+++
@@ -1,7 +0,0 @@
-const calculadora = require('./app'); //método para importar os modulos exportados do arquivo app.js
-
-console.log(calculadora.nome);
-console.log('soma: ', calculadora.soma(1,2));
-console.log('sub: ', calculadora.sub(5,3));
-console.log('multi: ', calculadora.multi(3,5));
-console.log('div: ',calculadora.div(15,3));
