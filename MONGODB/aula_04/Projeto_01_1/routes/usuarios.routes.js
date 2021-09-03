b61d5b75ab385b6207c6751fe39fa1b8efc32446
rfka <kaminski.lista@gmail.com>
2021-09-02 21:47:37 -0300

    Delete MONGODB directory

--- MONGODB/aula_04/Projeto_01_1/routes/usuarios.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// retorna a lista de usuarios
-router.get('/usuarios', () =>{
-    //logica para listar os usuarios
-})
-
-module.exports = router;
